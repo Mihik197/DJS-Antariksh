872979b8192f60fcca4b7828028dd95442bb8b34
Mihik197 <gea1500517@gmail.com>
2024-08-17 19:06:05 +0530

    task-2 debugging

---
+++ ROS2/serverclient/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'serverclient'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='your_email@example.com',
+    description='server-client model for turtlesim control',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'turtle_server = scripts.turtle_server:main',
+            'turtle_client = scripts.turtle_client:main',
+        ],
+    },
+)
