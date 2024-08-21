ae7b1bf4417e0ae449a7cd8e4e119fa2e3035579
Mihik197 <gea1500517@gmail.com>
2024-08-21 23:35:35 +0530

    removed redundant parts

--- ROS2/v2turtlebot_circular_motion/src/figure_eight_publisher.py
+++ ROS2/v2turtlebot_circular_motion/src/figure_eight_publisher.py
@@ -4,7 +4,6 @@ import math
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from turtlesim.msg import Pose
 
 class FigureEightPublisher(Node):
 
@@ -12,22 +11,10 @@ class FigureEightPublisher(Node):
         super().__init__('figure_eight_publisher')
 
         self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
-        self.subscription = self.create_subscription(Pose, '/turtle1/pose', self.pose_callback, 10)  # current coordinates
-
-        # spawn coordinates for pose message (just for initialization)
-        self.x = 0.0
-        self.y = 0.0
-        self.theta = 0.0
-
         self.timer = self.create_timer(0.1, self.move_infinity)        
 
         self.duration = 40  # duration to complete half loop
         self.start_time = self.get_clock().now().to_msg().sec
-
-    def pose_callback(self, pose):
-        self.x = pose.x
-        self.y = pose.y
-        self.theta = pose.theta
 
     def move_infinity(self):
         current_time = self.get_clock().now().to_msg().sec
